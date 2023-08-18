b066971d659170fd3b6e97be05ea84a6482301ae
Ayki <ayhanshahbazov@gmail.com>
2023-08-18 20:38:13 +0300

    Time: 53 ms (94.83%), Space: 43.2 MB (84.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    let x = 0;
-    while (2**x < n) {
-        x++;
-    }
-    return 2**x === n;
+    return (Math.log2(n) % 1 === 0);
         
 };
