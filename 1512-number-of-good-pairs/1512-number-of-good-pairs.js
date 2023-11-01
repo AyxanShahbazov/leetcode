1e5578324923e75ebc605f2ec0b0fdba7bfcd7bb
Ayki <ayhanshahbazov@gmail.com>
2023-11-02 00:58:06 +0200

    Time: 58 ms (19.18%), Space: 42.2 MB (27.21%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function numIdenticalPairs(A) {
+    let ans = 0;
+    const cnt = {};
+    
+    for (let x of A) {
+        ans += cnt[x] || 0;
+        cnt[x] = (cnt[x] || 0) + 1;
+    }
+    
+    return ans;
+}
